7bddfc058e3ff5f3a73bce5266ac44674e6e652f
FoxAzure <108664801+FoxAzure@users.noreply.github.com>
2025-03-11 16:13:23 -0300

    Criação da estrutura principal

---
+++ script.js
@@ -0,0 +1,16 @@
+const buttons = document.querySelectorAll('button');
+const iframe = document.getElementById('iframe');
+
+const links = [
+    { name: 'Resumo Pressões', link: 'https://app.powerbi.com/view?r=eyJrIjoiNzRkZDEwN2UtMGZlYS00OWFiLWFkMjQtMmQ4YWVjZjg1MDBlIiwidCI6IjdkYjQwZjM0LTk1MzYtNGJiNC1iODVhLThhZDViMjEwODU0OCJ9' },
+    { name: 'Avaliação Campo Gotejo', link: 'https://app.powerbi.com/view?r=eyJrIjoiZjk0YTNjNzQtNjNjOC00ZTgzLTg0YWMtZDAwZTE3ZTk2YmFmIiwidCI6IjdkYjQwZjM0LTk1MzYtNGJiNC1iODVhLThhZDViMjEwODU0OCJ9' },
+    { name: 'Avaliação Casa de bomba', link: 'https://app.powerbi.com/view?r=eyJrIjoiYjNjNzJiNDQtNTM4ZS00ODE0LTg2MWQtZjdhMTMxNmQ5YzRiIiwidCI6IjdkYjQwZjM0LTk1MzYtNGJiNC1iODVhLThhZDViMjEwODU0OCJ9' }
+];
+
+buttons.forEach((button, index) => {
+    button.addEventListener('click', () => {
+        buttons.forEach(btn => btn.classList.remove('selected'));
+        button.classList.add('selected');
+        iframe.src = links[index].link;
+    });
+});
